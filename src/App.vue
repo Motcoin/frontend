e2cccf25f0c60a90e5c10b05e459654e854479b5
Tom Pillokat <tom.pillokat@googlemail.com>
2021-02-15 15:24:56 +0100

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Tomcoin logo" src="./assets/Tomcoin_Logo.png" />
+  <div>
+    <p class="bg-cyan-400">
+    HELLOOOOOOOO
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="border">
+    <div class="index">
+
+    </div>
+    <div class="info">
+
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+props: {
+  block: {
+    type: Object,
+    required: true
+  },
+},
+});
+</script>
+
+<style>
+</style>
