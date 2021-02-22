bd9e6b8b67a9969510a4491640a7b807786ed8b9
Tom Pillokat <tom.pillokat@googlemail.com>
2021-02-22 14:12:24 +0100

    Add component to display one block

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="border">
+  <div class="border-2 border-black p-2">
     <div class="index">
-
+      {{block.index}}
+    </div>
+    <div class="truncate text-white px-2" :class="{'bg-blue-600': block.index % 2 === 0, 'bg-red-600': block.index % 2 === 1}">
+      {{block.previousHash || 'genesis block'}}
     </div>
     <div class="info">
-
+      {{block.data}}
+    </div>
+    <div>
+      {{block.nonce}} | d: {{block.difficulty}}
+    </div>
+    <div class="truncate text-white px-2" :class="{'bg-red-600': block.index % 2 === 0, 'bg-blue-600': block.index % 2 === 1}">
+      {{block.hash}}
     </div>
   </div>
 </template>
@@ -23,4 +32,12 @@ props: {
 </script>
 
 <style>
+.index{
+  font-size: 2rem;
+}
+.truncate {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
 </style>
